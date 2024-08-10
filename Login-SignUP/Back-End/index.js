b7977f5027d439648acf978dafe6e3fd0075c436
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2024-08-10 17:24:19 +0530

    Testing Backend

--- Login-SignUP/Back-End/index.js
+++ Login-SignUP/Back-End/index.js
@@ -3,10 +3,9 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping', (res,res)=>{
-    res.setEncoding('PONG')
-});
-
+app.get('/ping' , (res,res)=>{
+    res.send('Pong');
+})
 app.listen(PORT,()=>{
     console.log("Server is runnig on ${PORT}")
 })
