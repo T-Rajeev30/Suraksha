df1e662cb8fc05c839dd29533fac8a13ee7fba91
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2024-08-10 17:48:37 +0530

    nodeman installation

--- Login-SignUP/Back-End/index.js
+++ Login-SignUP/Back-End/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const PORT = process.env.PORT || 8080;
 
-app.get('/ping' , (res,res)=>{
-    res.send('Pong');
-})
-app.listen(PORT,()=>{
-    console.log("Server is runnig on ${PORT}")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
